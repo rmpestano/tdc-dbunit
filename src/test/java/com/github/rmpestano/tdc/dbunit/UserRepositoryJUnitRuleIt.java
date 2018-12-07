43a9dd061de3c9c28d89654812c6f69876a10c20
rafael-pestano <rafael-pestano@procergs.rs.gov.br>
2018-12-07 17:44:45 -0200

    perf

--- src/test/java/com/github/rmpestano/tdc/dbunit/UserRepositoryJUnitRuleIt.java
+++ src/test/java/com/github/rmpestano/tdc/dbunit/UserRepositoryJUnitRuleIt.java
@@ -27,7 +27,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
